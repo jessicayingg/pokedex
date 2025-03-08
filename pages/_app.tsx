350978a5978149959b8a8950cee26632c3649d69
jessicayingg <jessicaxying@gmail.com>
2025-03-08 01:31:19 -0500

    File organization

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,10 @@
 import "@/styles/globals.css";
-import "@/styles/infocard.css";
-import "@/styles/searchbar.css";
-import "@/styles/pokedex.css";
-import "@/styles/dpad.css";
+import "@/styles/pokedex-page/infocard.css";
+import "@/styles/pokedex-page/searchbar.css";
+import "@/styles/pokedex-page/pokedex.css";
+import "@/styles/pokedex-page/dpad.css";
 import "@/styles/navbar.css";
+import "@/styles/login-register-pages/login.css";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
