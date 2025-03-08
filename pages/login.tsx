9b399785a1a84ab878e0c3ea715eeb429a8109da
jessicayingg <jessicaxying@gmail.com>
2025-03-08 01:38:32 -0500

    Login and register pages made

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,5 +1,35 @@
 import Layout from "../components/Layout";
 
-const loginView = (props: any) => <Layout>{`Log-in page`}</Layout>;
+const loginView = () => {
+  return (
+    <Layout>
+      <div className="login-container">
+        <div className="login-text">Login</div>
+        <div className="login-username-container">
+          <div>Username</div>
+          <input
+            className="username-input"
+            type="text"
+            placeholder="Type your username"
+          ></input>
+        </div>
+        <div className="login-password-container">
+          <div>Password</div>
+          <input
+            className="password-input"
+            type="text"
+            placeholder="Type your password"
+          ></input>
+          <div className="forgot-password">Forgot password?</div>
+        </div>
+        <button className="login-button">Log in</button>
+        <div className="signup-container">
+          <div>New? Make an account</div>
+          <button className="signup-button">Sign up</button>
+        </div>
+      </div>
+    </Layout>
+  );
+};
 
 export default loginView;
--- pages/register.tsx
+++ pages/register.tsx
@@ -1,5 +1,35 @@
 import Layout from "../components/Layout";
 
-const registerView = (props: any) => <Layout>{`Register page`}</Layout>;
+const registerView = () => {
+  return (
+    <Layout>
+      <div className="login-container">
+        <div className="login-text">Register</div>
+        <div className="login-username-container">
+          <div>Username</div>
+          <input
+            className="username-input"
+            type="text"
+            placeholder="Type your username"
+          ></input>
+        </div>
+        <div className="login-password-container">
+          <div>Password</div>
+          <input
+            className="password-input"
+            type="text"
+            placeholder="Type your password"
+          ></input>
+          <div className="forgot-password">Forgot password?</div>
+        </div>
+        <button className="login-button">Log in</button>
+        <div className="signup-container">
+          <div>New? Make an account</div>
+          <button className="signup-button">Sign up</button>
+        </div>
+      </div>
+    </Layout>
+  );
+};
 
 export default registerView;
