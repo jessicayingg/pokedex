e2521b4ace16e205756d1afbf3baa52dcf8dab3a
jessicayingg <jessicaxying@gmail.com>
2025-03-14 01:17:58 -0400

    Styling-ish

--- components/Header.tsx
+++ components/Header.tsx
@@ -12,9 +12,33 @@ const Header = ({ navButtons, loggedIn, loggedInName }: HeaderProps) => {
   const router = useRouter();
   const curPage = router.pathname;
 
-  const profileButton = (name: String) => {
+  const putButtons = () => {
     if (loggedIn) {
-      return <button className="NavButton profile-button">{name}</button>;
+      return (
+        <div className="nav-buttons">
+          {navButtons.map((button) => (
+            <Button
+              key={button.label}
+              navButton={button}
+              activePage={curPage}
+            />
+          ))}
+          <button className="NavButton profile-button">{loggedInName}</button>
+          <button className="NavButton log-in-button">Log out</button>
+        </div>
+      );
+    } else {
+      return (
+        <div className="nav-buttons">
+          {navButtons.map((button) => (
+            <Button
+              key={button.label}
+              navButton={button}
+              activePage={curPage}
+            />
+          ))}
+        </div>
+      );
     }
   };
 
@@ -25,12 +49,7 @@ const Header = ({ navButtons, loggedIn, loggedInName }: HeaderProps) => {
           <img className="logo-icon" src="images/pokeball.png"></img>
         </a>
       </div>
-      <div className="nav-buttons">
-        {navButtons.map((button) => (
-          <Button key={button.label} navButton={button} activePage={curPage} />
-        ))}
-        {profileButton(loggedInName)}
-      </div>
+      {putButtons()}
     </div>
   );
 };
