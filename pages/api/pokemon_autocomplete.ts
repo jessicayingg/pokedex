2f75224fb2e890d66c07237a98e51d6db8e9b6aa
jessicayingg <jessicaxying@gmail.com>
2025-05-26 09:10:45 -0400

    Comments

--- pages/api/pokemon_autocomplete.ts
+++ pages/api/pokemon_autocomplete.ts
@@ -6,27 +6,33 @@ type PokemonData = {
   name: string;
 };
 
+// API handler
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<PokemonData[] | { error: string }>
+  // responsds with either list of pokemon or an error object
 ) {
   //const { query } = req.query; // Get query from request
   const query = req.query.query as string;
 
   try {
+    // Fetch all pokemon from pokeapi
     const response = await fetch(
       `https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0`
     );
+    // error
     if (!response.ok) {
       throw new Error("Failed to fetch Pokémon");
     }
-
+    // get json from fetched data
     const data = await response.json();
 
     const filteredResults = data.results
+      // Filter by the query typed
       .filter((pokemon: { name: string }) =>
         pokemon.name.toLowerCase().startsWith(query.toLowerCase())
       )
+      // Filter out the special pokemon forms (i.e pokemon with id higher than 10000)
       .filter((pokemon: { url: string }) => {
         const id = Number(pokemon.url.split("/").slice(-2, -1)[0]);
         return id < 10000;
@@ -56,8 +62,10 @@ export default async function handler(
       name: data.name,
     }; */
 
+    // Success! Return results (list) and 200 status
     res.status(200).json(results);
   } catch (error) {
+    // Error, return error object and 500 status
     res.status(500).json({ error: (error as Error).message });
   }
 }
