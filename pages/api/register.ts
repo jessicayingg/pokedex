295bc8bb7e6071493e26c42ccab7478cbe90abc7
jessicayingg <jessicaxying@gmail.com>
2025-05-27 19:59:44 -0400

    Comments

--- pages/api/register.ts
+++ pages/api/register.ts
@@ -1,6 +1,6 @@
 import { NextApiRequest, NextApiResponse } from "next";
-import { connectToDB } from "../../lib/dbConnect"; // Import your DB connection
-import User from "../../models/User"; // You'll create this model next
+import { connectToDB } from "../../lib/dbConnect";
+import User from "../../models/User"; 
 import bcrypt from "bcryptjs";
 
 export default async function handler(
@@ -16,7 +16,7 @@ export default async function handler(
       password,
     });
 
-    // 1. Validate input
+    // Check input
     if (!name || !email || !password) {
       console.log("Validation failed: missing email or password");
       return res
@@ -26,11 +26,11 @@ export default async function handler(
 
     try {
       console.log("Connecting to the database...");
-      // 2. Connect to the database
+      // Connect to the database
       await connectToDB();
 
       console.log("Checking if user already exists with email:", email);
-      // 3. Check if the email already exists
+      // Check if the email already exists
       const existingUser = await User.findOne({ email });
       if (existingUser) {
         console.log("User already exists with email:", email);
@@ -38,11 +38,11 @@ export default async function handler(
       }
 
       console.log("Hashing the password for user:", email);
-      // 3. Hash the password before saving the user
-      const hashedPassword = await bcrypt.hash(password, 10); // 10 is the salt rounds
+      // Hash the password before saving the user
+      const hashedPassword = await bcrypt.hash(password, 10);
 
       console.log("Creating new user with email:", email);
-      // 4. Create the user without hashing the password
+      // Create user
       const newUser = new User({
         name,
         email,
@@ -50,11 +50,11 @@ export default async function handler(
       });
 
       console.log("Saving new user to the database...");
-      // 5. Save the user to the database
+      // Save the user to the database
       await newUser.save();
 
       console.log("User registered successfully:", { email, name });
-      // 6. Send success response
+      // Success!
       return res.status(201).json({ message: "User registered successfully" });
     } catch (error) {
       console.error("Error registering user:", error);
